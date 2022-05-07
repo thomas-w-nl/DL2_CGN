ac046002a69529ef42f9e078e1345380378a143e
thomas <thomas.wiggers@student.uva.nl>
2022-05-07 22:57:19 +0200

    dataset creation script

--- imagenet/create_dataset.py
+++ imagenet/create_dataset.py
@@ -1,8 +1,14 @@
+import os
+
 import torch
 import torch.nn.functional as F
 import argparse
 
 import repackage
+from PIL import Image
+from torchvision import transforms
+import tqdm
+
 repackage.up()
 
 from imagenet.models import CGN
@@ -17,6 +23,10 @@ def generate_images(args):
         pretrained=False,
     )
 
+    dataset_path = args.dataset_path + "/"
+
+    os.makedirs(dataset_path, exist_ok=True)
+
 
     weights = torch.load(args.weights_path, map_location="cpu")
     weights = {k.replace("module.", ""): v for k, v in weights.items()}
@@ -25,7 +35,7 @@ def generate_images(args):
 
     data_dict = {"bg": None, "fg": None, "m": None, "gt": None, "labels": None}
     with torch.no_grad():
-        for i in range(args.n_data):
+        for i in tqdm.trange(args.n_data):
 
             y_vec = torch.randint(0, 1000, (1,)).to(torch.int64)
             y_vec = F.one_hot(y_vec, 1000).to(torch.float32)
@@ -34,23 +44,36 @@ def generate_images(args):
             u_vec = cgn.get_noise_vec()
 
             inp = (u_vec.to(dev), y_vec.to(dev), cgn.truncation)
-            print(cgn.truncation)
             x_gt, mask, _, foreground, background, _ = cgn(inp=inp)
 
-            if i:
-                data_dict["bg"] = torch.cat((background.cpu(), data_dict["bg"]), dim=0)
-                data_dict["fg"] = torch.cat((foreground.cpu(), data_dict["fg"]), dim=0)
-                data_dict["m"] = torch.cat((mask.cpu(), data_dict["m"]), dim=0)
-                data_dict["gt"] = torch.cat((x_gt.cpu(), data_dict["gt"]), dim=0)
-                data_dict["labels"] = torch.cat((y_vec, data_dict["labels"]), dim=0)
-            else:
-                data_dict["labels"] = y_vec.cpu()
-                data_dict["bg"] = background.cpu()
-                data_dict["fg"] = foreground.cpu()
-                data_dict["m"] = mask.cpu()
-                data_dict["gt"] = x_gt.cpu()
+            x_gt = x_gt.clip(0,1).squeeze(0)
+            mask = mask.clip(0,1).squeeze(0)
+            foreground = foreground.clip(0,1).squeeze(0)
+            background = background.clip(0,1).squeeze(0)
 
-        print(data_dict["gt"].shape)
+            to_pil = transforms.ToPILImage()
+
+            to_pil(x_gt).save(dataset_path + f"{i}_gt.png")
+            to_pil(mask).save(dataset_path + f"{i}_mask.png")
+            to_pil(foreground).save(dataset_path + f"{i}_fg.png")
+            to_pil(background).save(dataset_path + f"{i}_bg.png")
+
+
+
+        #     if i:
+        #         data_dict["bg"] = torch.cat((background.cpu(), data_dict["bg"]), dim=0)
+        #         data_dict["fg"] = torch.cat((foreground.cpu(), data_dict["fg"]), dim=0)
+        #         data_dict["m"] = torch.cat((mask.cpu(), data_dict["m"]), dim=0)
+        #         data_dict["gt"] = torch.cat((x_gt.cpu(), data_dict["gt"]), dim=0)
+        #         data_dict["labels"] = torch.cat((y_vec, data_dict["labels"]), dim=0)
+        #     else:
+        #         data_dict["labels"] = y_vec.cpu()
+        #         data_dict["bg"] = background.cpu()
+        #         data_dict["fg"] = foreground.cpu()
+        #         data_dict["m"] = mask.cpu()
+        #         data_dict["gt"] = x_gt.cpu()
+        #
+        # print(data_dict["gt"].shape)
 
     return
 
@@ -66,6 +89,8 @@ if __name__ == "__main__":
                         help='How many datapoints to sample'),
     parser.add_argument('--weights_path', type=str, default="imagenet/weights/cgn.pth",
                         help='Which weights to load for the CGN')
+    parser.add_argument('--dataset_path', type=str,  required=True,
+                        help='Where to place the dataset')
     parser.add_argument('--truncation', type=float, default=1.0,
                         help='Truncation value for the sampling the noise')
 
