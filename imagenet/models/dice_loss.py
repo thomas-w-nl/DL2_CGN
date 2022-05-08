345c558255398db33433fb250bcc416fcc9e441d
thomas <thomas.wiggers@student.uva.nl>
2022-05-09 00:17:11 +0200

    fix dice loss https://github.com/pytorch/pytorch/issues/1249

--- imagenet/models/dice_loss.py
+++ imagenet/models/dice_loss.py
@@ -1,16 +1,16 @@
 import torch
 
-def DiceLoss(input, target):
 
+def DiceLoss(input, target):
     smoothing = 1
 
     input_flat = input.view(-1)
     target_flat = target.view(-1)
 
-    numerator = 2 * (input_flat * target_flat) + smoothing
+    numerator = 2 * (input_flat * target_flat).sum() + smoothing
 
     denom = input_flat.sum() + target_flat.sum() + smoothing
 
-    loss = numerator/denom
+    loss = 1 - (numerator / denom)
 
     return loss
