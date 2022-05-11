b55e6162186f1276600690497db374137f07a419
thomas <thomas.wiggers@student.uva.nl>
2022-05-11 23:56:46 +0200

    veel

--- imagenet/models/__init__.py
+++ imagenet/models/__init__.py
@@ -1,9 +1,9 @@
 from imagenet.models.biggan import BigGAN
 from imagenet.models.dice_loss import DiceLoss
-from imagenet.models.u2net import U2NET, RefineNetShallow
+from imagenet.models.u2net import U2NET, RefineNetShallow, U2NETP
 from imagenet.models.cgn import CGN
 from imagenet.models.classifier_ensemble import InvariantEnsemble
 
 __all__ = [
-    CGN, InvariantEnsemble, BigGAN, U2NET, DiceLoss, RefineNetShallow
+    CGN, InvariantEnsemble, BigGAN, U2NET, DiceLoss, RefineNetShallow, U2NETP
 ]
--- imagenet/train_gan.py
+++ imagenet/train_gan.py
@@ -9,6 +9,7 @@ from os.path import join
 from datetime import datetime
 import argparse
 
+import PIL
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -30,8 +31,18 @@ from utils import toggle_grad
 from shared.losses import PerceptualLoss
 import torchvision.models as models
 
+
 def get_real_dataloader(args, n):
-    imagenet = torchvision.datasets.ImageNet('.')
+    # imagenet = torchvision.datasets.ImageNet("/storage/twiggers/",
+    #                                          transform=torchvision.transforms.Resize((256, 256)))
+
+    tf = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((256, 256)),
+        torchvision.transforms.ToTensor()])
+
+    imagenet = torchvision.datasets.CIFAR10("imagenet/data/cifar/",
+                                            transform=tf, download=True)
+
     np.random.seed(69)
     imagenet_n = torch.utils.data.Subset(imagenet, np.random.choice(len(imagenet), n, replace=False))
 
@@ -44,6 +55,7 @@ def get_real_dataloader(args, n):
         num_workers=4, pin_memory=False, drop_last=True)
 
     return loader
+
 
 def get_fake_dataloader(args):
     dataset = RefinementDataset(args.dataset)
@@ -62,7 +74,9 @@ def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Setup GAN network
-    model_d = models.resnet18(pretrained=True)
+    model_d = models.resnet18(pretrained=False)
+    model_d.fc = nn.Linear(512 * 1, 2)
+
     model_g = U2NETP(3, 3)
 
     model_d.to(device)
@@ -72,12 +86,17 @@ def main(args):
     toggle_grad(model_g, True)
 
     # Setup training utilities
-    discriminator_optimizer = torch.optim.Adam(model_d.parameters(), lr=args.lr)
+    discriminator_optimizer = torch.optim.Adam(model_d.parameters(), lr=args.lr * 0.1)
     generator_optimizer = torch.optim.Adam(model_g.parameters(), lr=args.lr)
 
     scheduler = torch.optim.lr_scheduler.MultiStepLR(discriminator_optimizer, milestones=args.reduce_lr_on, gamma=0.1)
 
     criterion = nn.CrossEntropyLoss()
+
+    fake_dataloder1 = get_fake_dataloader(args)
+    fake_dataloder2 = get_fake_dataloader(args)
+    real_loader = get_real_dataloader(args, len(fake_dataloder1.dataset))
+    # real_loader = get_fake_dataloader(args)
 
     # logging
     run = wandb.init(project="dl2-refinement", entity="thomas-w",
@@ -85,16 +104,49 @@ def main(args):
                      # group=args.model,
                      notes=f"{args.notes}", reinit=True)
 
-    fake_dataloder1 = get_fake_dataloader(args)
-    fake_dataloder2 = get_fake_dataloader(args)
-    real_loader = get_real_dataloader(args, len(fake_dataloder1.dataset))
+    print("Pretraining Unet")
+    pretrain_optimizer = torch.optim.Adam(model_g.parameters(), lr = 1e-2)
+    for i in range(10):
+
+        criterion_pretrain = nn.MSELoss()
+        total = 0
+        for i, img in enumerate(fake_dataloder1):
+            img = img.to(device)
+            # Generator gradient step
+            pretrain_optimizer.zero_grad()
+            images_fake = model_g(img)
+            loss = criterion_pretrain(images_fake, img)
+            loss.backward()
+            pretrain_optimizer.step()
+            total += loss.item()
+
+            # Pretraining is done quite quickly
+            if i == 500:
+                break
+        loss = total / len(fake_dataloder1)
+        print("Loss", loss)
+        if i == 6:
+            pretrain_optimizer = torch.optim.Adam(model_g.parameters(), lr=1e-3)
+
+        # im = torchvision.utils.make_grid(torch.cat((images_fake.cpu(), img.cpu())), ).clip(0, 1).permute((1, 2, 0))
+        # plt.imshow(im)
+        # plt.show()
+
+    torch.save(model_g.state_dict(), "u2net_pretraining.pt")
+
+    print("Loaded pretrained unet")
+    model_g.load_state_dict(torch.load("u2net_pretraining.pt"))
+
+
 
     # generate data
     for epoch in trange(args.epochs):
         loss_total_discriminator = []
         loss_total_generator = []
+        total_accuracy = []
+        last_total_accuracy = .5
 
-        for fake_1, fake_2, real in zip(fake_dataloder1, fake_dataloder2, real_loader):
+        for fake_1, fake_2, (real, _) in zip(fake_dataloder1, fake_dataloder2, real_loader):
             x_gen1 = fake_1.to(device)
             x_gen2 = fake_2.to(device)
             real = real.to(device)
@@ -102,32 +154,35 @@ def main(args):
             # Create fake images
             with torch.no_grad():
                 images_fake = model_g(x_gen1)
-            labels_fake = torch.zeros((images_fake.shape[0], 1))
+            labels_fake = torch.zeros((images_fake.shape[0],), device=device).long()
 
             # append fake and real
-            combined_real_fake = np.concatenate([images_fake, real])
-            combined_labels = np.concatenate([labels_fake, labels_fake+1])
+            combined_real_fake = torch.cat([images_fake, real])
+            combined_labels = torch.cat([labels_fake, labels_fake + 1])
 
             # Discriminator gradient step
             discriminator_optimizer.zero_grad()
             pred = model_d(combined_real_fake)
-
             loss = criterion(pred, combined_labels)
             loss.backward()
-            discriminator_optimizer.step()
+
 
             loss_total_discriminator.append(loss.cpu().item())
+            accuracy = torch.sum(pred.argmax(1) == combined_labels).detach().cpu() / len(combined_labels)
+            total_accuracy.append(accuracy)
 
+            if accuracy < .8:
+                discriminator_optimizer.step()
 
             # Generator gradient step
             generator_optimizer.zero_grad()
 
-
             images_fake = model_g(x_gen2)
-            labels_fake = torch.zeros((images_fake.shape[0], 1))
+            labels_fake = torch.zeros((images_fake.shape[0],), device=device).long()
 
-            with torch.no_grad():
-                pred = model_d(images_fake)
+            toggle_grad(model_d, False)
+            pred = model_d(images_fake)
+            toggle_grad(model_d, True)
 
             loss = criterion(pred, labels_fake)
             loss.backward()
@@ -135,21 +190,28 @@ def main(args):
 
             loss_total_generator.append(loss.cpu().item())
 
-
         # lr = scheduler.get_last_lr()
         lr = discriminator_optimizer.param_groups[0]['lr']
         scheduler.step()
 
         loss_generator = np.mean(loss_total_generator)
         loss_discriminator = np.mean(loss_total_discriminator)
-        print("avg loss_generator:    ", loss_generator)
-        print("avg loss_discriminator:", loss_discriminator)
+        total_accuracy = np.mean(total_accuracy)
+        last_total_accuracy = total_accuracy
+        # print("avg loss_generator:    ", loss_generator)
+        # print("avg loss_discriminator:", loss_discriminator)
 
+        image = wandb.Image(torchvision.utils.make_grid(images_fake).clip(0, 1))
+        # print("image.shape", image.shape)
+        #
         log = {
             "epoch": epoch,
             "loss_generator": loss_generator,
             "loss_discriminator": loss_discriminator,
+            "discriminator_accuracy": total_accuracy,
             "lr": lr,
+            "images": image,
+
         }
         wandb.log(log)
 
@@ -168,7 +230,7 @@ if __name__ == '__main__':
     #                     choices=['random', 'best_classes', 'fixed_classes'],
     #                     help='Choose between random sampling, sampling from the best ' +
     #                          'classes or the classes passed to args.classes')
-    parser.add_argument('--epochs', type=int, default=30,
+    parser.add_argument('--epochs', type=int, default=100,
                         help='How many epochs to train for')
     parser.add_argument('--lr', type=float, default=1e-3,
                         help='Learning rate')
@@ -178,8 +240,8 @@ if __name__ == '__main__':
                         help='Notes for wandb')
     parser.add_argument('--dataset', type=str, default="imagenet/data/refinement1/",
                         help='Path to the dataset')
-    parser.add_argument('--batch_sz', type=int, default=8,
-                        help='Batch size, default 32')
+    parser.add_argument('--batch_sz', type=int, default=16,
+                        help='Batch size, default 16')
 
     args = parser.parse_args()
 
