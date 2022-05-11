b5bf6ca47ae34c4337b0eab197d053dd443b1741
thomas <thomas.wiggers@student.uva.nl>
2022-05-11 15:53:29 +0200

    setup  training

--- imagenet/train_gan.py
+++ imagenet/train_gan.py
@@ -37,69 +37,12 @@ def get_imagenet_data(n):
 
     return imagenet_n
 
-def save_image(im, path):
-    torchvision.utils.save_image(im.detach().cpu(), path, normalize=True)
+def discriminator_train_batch(model, batch):
 
 
-# Lists of best or most interesting shape/texture/background classes
-# (Yes, I know all imagenet classes very well by now)
-MASKS = [9, 18, 22, 35, 56, 63, 96, 97, 119, 207, 225, 260, 275, 323, 330, 350, 370, 403, 411,
-         414, 427, 438, 439, 441, 460, 484, 493, 518, 532, 540, 550, 559, 561, 570, 604, 647,
-         688, 713, 724, 749, 751, 756, 759, 779, 780, 802, 814, 833, 841, 849, 850, 859, 869,
-         872, 873, 874, 876, 880, 881, 883, 894, 897, 898, 900, 907, 930, 933, 945, 947, 949,
-         950, 953, 963, 966, 967, 980]
-FOREGROUND = [12, 15, 18, 25, 54, 66, 72, 130, 145, 207, 251, 267, 271, 275, 293, 323, 385,
-              388, 407, 409, 427, 438, 439, 441, 454, 461, 468, 482, 483, 486, 490, 492, 509,
-              530, 555, 607, 608, 629, 649, 652, 681, 688, 719, 720, 728, 737, 741, 751, 756,
-              779, 800, 810, 850, 852, 854, 869, 881, 907, 911, 930, 936, 937, 938, 941, 949,
-              950, 951, 954, 957, 959, 963, 966, 985, 987, 992]
-BACKGROUNDS = [7, 9, 20, 30, 35, 46, 50, 65, 72, 93, 96, 97, 119, 133, 147, 337, 350, 353, 354,
-               383, 429, 460, 693, 801, 888, 947, 949, 952, 953, 955, 958, 970, 972, 973, 974,
-               977, 979, 998]
+def refinement_train_batch():
+    pass
 
-
-def sample_classes(mode, classes=None):
-    if mode == 'random':
-        return np.random.randint(0, 1000, 3).tolist()
-
-    elif mode == 'best_classes':
-        return [np.random.choice(MASKS),
-                np.random.choice(FOREGROUND),
-                np.random.choice(BACKGROUNDS)]
-
-    elif mode == 'fixed_classes':
-        return [int(c) for c in classes]
-
-    else:
-        assert ValueError("Unknown sample mode {mode}")
-
-
-def show_images(x_gt, x_gen, x_gen_refined):
-    a = x_gt
-    b = x_gen
-    c = x_gen_refined
-
-    # a = unnormalize(a)
-    # b = unnormalize(b)
-    # c = unnormalize(c)
-
-    a = a.detach().cpu()[0].permute(1, 2, 0).numpy().clip(0, 1)
-    b = b.detach().cpu()[0].permute(1, 2, 0).numpy().clip(0, 1)
-    c = c.detach().cpu()[0].permute(1, 2, 0).numpy().clip(0, 1)
-
-    fig, axs = plt.subplots(2, 2)
-    for x in axs.ravel():
-        x.axis('off')
-
-    axs[0, 0].imshow(a)
-    axs[0, 0].set_title("x_gt")
-
-    axs[1, 0].imshow(b)
-    axs[1, 0].set_title("x_gen")
-
-    axs[1, 1].imshow(c)
-    axs[1, 1].set_title("x_gen_refined")
-    plt.show()
 
 
 def main(args):
@@ -116,17 +59,13 @@ def main(args):
     toggle_grad(model_g, True)
 
     # Setup training utilities
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    discriminator_optimizer = torch.optim.Adam(model_d.parameters(), lr=args.lr)
+    generator_optimizer = torch.optim.Adam(model_g.parameters(), lr=args.lr)
 
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.reduce_lr_on, gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(discriminator_optimizer, milestones=args.reduce_lr_on, gamma=0.1)
 
-    if args.dice_loss:
-        criterion = DiceLoss
-    else:
-        criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
 
-    criterion = PerceptualLoss(style_wgts=[4, 4, 4, 4])
-    criterion.to(device)
 
     dataset = RefinementDataset(args.dataset)
 
@@ -156,27 +95,48 @@ def main(args):
 
             x_gen = mask * foreground + (1 - mask) * background
 
-            input = torch.hstack((mask * foreground, (1 - mask) * background))
+            # Create fake images
+            with torch.no_grad():
+                images_fake = model_g(x_gen)
+            labels_fake = torch.zeros((images_fake.shape[0], 1))
 
-            input = input.detach()
-            x_gt = x_gt.detach()
 
-            optimizer.zero_grad()
+            # append fake and real
 
-            x_gen_ref = model(input)
 
-            loss = criterion(x_gen_ref, x_gt)
+            # Discriminator gradient step
+            discriminator_optimizer.zero_grad()
+            pred = model_d(fake and real)
+
+            loss = criterion(pred, labels)
             loss.backward()
-            optimizer.step()
+            discriminator_optimizer.step()
 
-            loss_total.append(loss.cpu().item())
+            # loss_total.append(loss.cpu().item())
+
+
+            # Generator gradient step
+            generator_optimizer.zero_grad()
+
+            images_fake = model_g(x_gen) # TODO NIEUWE PLAATJES
+            labels_fake = torch.zeros((images_fake.shape[0], 1))
+
+            with torch.no_grad():
+                pred = model_d(images_fake)
+
+
+            loss = criterion(pred, labels_fake)
+            loss.backward()
+            generator_optimizer.step()
+
+
 
         if os.getlogin() == "thomas":
             if x_gt is not None:
                 show_images(x_gt, x_gen, x_gen_ref)
 
         # lr = scheduler.get_last_lr()
-        lr = optimizer.param_groups[0]['lr']
+        lr = discriminator_optimizer.param_groups[0]['lr']
         scheduler.step()
 
         loss = np.mean(loss_total)
