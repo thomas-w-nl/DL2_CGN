8af7e5ce61172b7dd5e28389ef810e33a1e6c255
RomanOort <r.s.oort@gmail.com>
2022-05-23 15:40:29 +0200

    Evaluation, accuracy comparison not yet working, for some reason

--- imagenet/eval.py
+++ imagenet/eval.py
@@ -1,19 +1,83 @@
 import argparse
 
 import sklearn.metrics
-from train_gan import get_real_dataloader
-import os
 import torch
 import numpy as np
-import torchvision
+import os
+from torchvision import transforms
 from train_classifier import accuracy
-from dataloader import ImagenetVanilla
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+from models.classifier_ensemble import InvariantEnsemble
+from dataloader import get_imagenet_dls
+import glob
+from PIL import Image
 
+def transform_labels(x):
+    return torch.tensor(x).to(torch.int64)
+
+class ImagenetA(Dataset) :
+
+    def __init__(self, root_path):
+        super(ImagenetA, self).__init__()
+        # root = join('.', 'imagenet', 'data')
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+
+        # Transforms
+        t_list = [transforms.Resize(256), transforms.CenterCrop(224)]
+
+        t_list += [transforms.ToTensor(), normalize]
+        self.T_ims = transforms.Compose(t_list)
+
+        self.im_paths, self.labels = self.get_data(root_path)
+
+    def set_len(self, n):
+        assert n < len(self), "Ratio is too large, not enough CF data available"
+        self.im_paths = self.im_paths[:n]
+        self.labels = self.labels[:n]
+
+    @staticmethod
+    def get_data(p):
+        ims, labels = [], []
+        subdirs = sorted(glob.glob(p + '/*'))
+        for i, sub in enumerate(subdirs):
+            im = sorted(glob.glob(sub + '/*'))
+            l = np.ones(len(im))*i
+            ims.append(im), labels.append(l)
+        return np.concatenate(ims), np.concatenate(labels)
+
+    def __getitem__(self, idx):
+        ims = Image.open(self.im_paths[idx]).convert('RGB')
+        labels = self.labels[idx]
+        return {
+            'ims': self.T_ims(ims),
+            'labels': transform_labels(labels),
+        }
+
+    def __len__(self):
+        return len(self.im_paths)
+
+def accuracyA(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        print(pred)
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 def get_imagenetA(batch_size, data_root_path, workers):
     # dataset
-    dataset = ImagenetVanilla(data_root_path, train=False)
+    dataset = ImagenetA(data_root_path)
 
     # dataloader
     loader = DataLoader(dataset, batch_size=batch_size, num_workers=workers,
@@ -22,56 +86,78 @@ def get_imagenetA(batch_size, data_root_path, workers):
     return loader
 
 def eval(args):
-    model = torch.load(args.model_path)
+    #model = torch.load(args.model_path)
+
+    model = InvariantEnsemble(arch=args.model_name, pretrained=False, cut=1)
+
+    state_dict = torch.load(args.model_path)['state_dict']
+
+    state_dict = {state_dict_key.replace("module.", ""):state_dict_value for state_dict_key, state_dict_value in state_dict.items()}
+
+    model.load_state_dict(state_dict)
 
     model.eval()
 
-    datasets = args.datasets
     eval_dict = dict()
-    for path in datasets:
-        data_name = path.split("/")[-1]
-        single_dataset_eval = dict()
-        _, dataloader, _ = get_imagenetA(args.batch_size, path, args.workers)
 
-        top1, top5, AUPR = [], [], []
+    data_path = "../"*4 + args.dataset
 
-        for image_batch, label_batch in dataloader:
-            preds = model(image_batch)
-            acc1, acc5 = accuracy(preds, label_batch, topk=(1, 5))
-            avg_prec_score = sklearn.metrics.average_precision_score(label_batch.numpy(), preds.numpy())
+    if args.data_name == "imagenet":
+        _, dataloader, _ = get_imagenet_dls(False, args.batch_size, args.workers, data_path)
+    else:
+        dataloader = get_imagenetA(args.batch_size, data_path, args.workers)
 
-            top1.append(acc1)
-            top5.append(acc5)
-            AUPR.append(avg_prec_score)
-            print(acc1)
-            print(acc5)
-            print(avg_prec_score)
-            break
+    top1, top5, AUPR = [], [], []
 
-        single_dataset_eval["top1"] = np.mean(top1)
-        single_dataset_eval["top5"] = np.mean(top5)
-        single_dataset_eval["AUPR"] = np.mean(AUPR)
+    for batch in dataloader:
+        image_batch = batch["ims"]
+        label_batch = batch["labels"]
+
+        print(label_batch)
+
+        preds = model(image_batch)
 
 
-        eval_dict[data_name] = single_dataset_eval
+        acc1_shape, acc5_shape = accuracyA(preds['shape_preds'], label_batch, topk=(1, 5))
+        acc1_text, acc5_text = accuracyA(preds['texture_preds'], label_batch, topk=(1, 5))
+        acc1_bg, acc5_bg = accuracyA(preds['bg_preds'], label_batch, topk=(1, 5))
 
-        print(args.model_name)
-        print("--------------")
-        print(f"Top-1 Accuracy: {np.mean(top1)}")
-        print(f"Top-5 Accuracy: {np.mean(top5)}")
-        print(f"AUPR-score: {np.mean(AUPR)}")
+        #avg_prec_score = sklearn.metrics.average_precision_score(label_batch.numpy(), preds.numpy())
+
+        # top1.append(acc1)
+        # top5.append(acc5)
+        # AUPR.append(avg_prec_score)
+        print(acc1_shape.item(), acc5_shape.item())
+        print(acc1_text.item(), acc5_text.item())
+        print(acc1_bg.item(), acc5_bg.item())
+        break
+
+    eval_dict["top1"] = np.mean(top1)
+    eval_dict["top5"] = np.mean(top5)
+    eval_dict["AUPR"] = np.mean(AUPR)
+
+    print(args.model_name)
+    print("--------------")
+    print(f"Top-1 Accuracy: {np.mean(top1)}")
+    print(f"Top-5 Accuracy: {np.mean(top5)}")
+    print(f"AUPR-score: {np.mean(AUPR)}")
 
     return eval_dict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--dataset", nargs="+", default="/storage/twiggers/imagenet-a",
+    parser.add_argument("--dataset", type=str, default="storage/twiggers/imagenet-a",
                         help="Path(s) to the dataset(s)")
-    parser.add_argument("--model_path", type=str, default="model/path",
+    parser.add_argument("--data_name", type=str, default="imagenetA")
+    parser.add_argument("--model_name", type=str, default="resnet50",
+                        help="Name of model architecture")
+    parser.add_argument("--model_path", type=str, default="imagenet/experiments/classifier_2022_05_23_09_39_classifier_INS2_LR_START_0_001_seed_2/model_best.pth",
                         help="Path to the model")
     parser.add_argument("--batch_size", type=int, default=16,
                         help="Batch size")
+    parser.add_argument("--workers", type=int, default=6,
+                        help="Number of workers for creating dataloader")
 
 
     args = parser.parse_args()
