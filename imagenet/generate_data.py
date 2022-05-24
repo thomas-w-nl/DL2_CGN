ca2dc3381c0730af1e8925480be3ae3320d0edb7
thomas <thomas.wiggers@student.uva.nl>
2022-05-24 12:28:40 +0200

    add option to only sample imagenetA classes

--- imagenet/generate_data.py
+++ imagenet/generate_data.py
@@ -150,6 +150,9 @@ BACKGROUNDS = [7, 9, 20, 30, 35, 46, 50, 65, 72, 93, 96, 97, 119, 133, 147, 337,
                383, 429, 460, 693, 801, 888, 947, 949, 952, 953, 955, 958, 970, 972, 973, 974,
                977, 979, 998]
 
+IMAGENET_A_CLASSES = [6, 11, 13, 15, 17, 22, 23, 27, 30, 37, 39, 42, 47, 50, 57, 70, 71, 76, 79, 89, 90, 94, 96, 97, 99, 105, 107, 108, 110, 113, 124, 125, 130, 132, 143, 144, 150, 151, 207, 234, 235, 254, 277, 283, 287, 291, 295, 298, 301, 306, 307, 308, 309, 310, 311, 313, 314, 315, 317, 319, 323, 324, 326, 327, 330, 334, 335, 336, 347, 361, 363, 372, 378, 386, 397, 400, 401, 402, 404, 407, 411, 416, 417, 420, 425, 428, 430, 437, 438, 445, 456, 457, 461, 462, 470, 472, 483, 486, 488, 492, 496, 514, 516, 528, 530, 539, 542, 543, 549, 552, 557, 561, 562, 569, 572, 573, 575, 579, 589, 606, 607, 609, 614, 626, 627, 640, 641, 642, 643, 658, 668, 677, 682, 684, 687, 701, 704, 719, 736, 746, 749, 752, 758, 763, 765, 768, 773, 774, 776, 779, 780, 786, 792, 797, 802, 803, 804, 813, 815, 820, 823, 831, 833, 835, 839, 845, 847, 850, 859, 862, 870, 879, 880, 888, 890, 897, 900, 907, 913, 924, 932, 933, 934, 937, 943, 945, 947, 951, 954, 956, 957, 959, 971, 972, 980, 981, 984, 986, 987, 988]
+
+
 
 def sample_classes(mode, classes=None):
     if mode == 'random':
@@ -159,6 +162,11 @@ def sample_classes(mode, classes=None):
         return [np.random.choice(MASKS),
                 np.random.choice(FOREGROUND),
                 np.random.choice(BACKGROUNDS)]
+
+    elif mode == 'imagenet_a':
+        return [np.random.choice(IMAGENET_A_CLASSES),
+                np.random.choice(IMAGENET_A_CLASSES),
+                np.random.choice(IMAGENET_A_CLASSES)]
 
     elif mode == 'fixed_classes':
         return [int(c) for c in classes]
@@ -212,13 +220,13 @@ def main(args):
                 x_gt, mask, premask, foreground, background, bg_mask = cgn(ys=ys)
                 x_gen = mask * foreground + (1 - mask) * background
 
-                x_gen_np = x_gen.cpu().squeeze(0).permute(1,2,0).numpy().clip(0,1)
-                x_gt_np = x_gt.cpu().squeeze(0).permute(1,2,0).numpy().clip(0,1)
-
-                fig, axs = plt.subplots(1, 2)
-                axs[0].imshow(x_gen_np)
-                axs[1].imshow(x_gt_np)
-                plt.show()
+                # x_gen_np = x_gen.cpu().squeeze(0).permute(1,2,0).numpy().clip(0,1)
+                # x_gt_np = x_gt.cpu().squeeze(0).permute(1,2,0).numpy().clip(0,1)
+                #
+                # fig, axs = plt.subplots(1, 2)
+                # axs[0].imshow(x_gen_np)
+                # axs[1].imshow(x_gt_np)
+                # plt.show()
                 # save image
                 # to save other outputs, simply add a line in the same format, e.g.:
                 # save_image(premask, join(ims_path, im_name + '_premask.jpg'))
@@ -232,7 +240,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, required=True,
-                        choices=['random', 'best_classes', 'fixed_classes'],
+                        choices=['random', 'best_classes', 'fixed_classes', 'imagenet_a'],
                         help='Choose between random sampling, sampling from the best ' +
                              'classes or the classes passed to args.classes')
     parser.add_argument('--n_data', type=int, required=True,
